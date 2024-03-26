e8a1900e734ce903fc8b9ee81a8802fdb412fd53
Nanguti <g.nanguti@gmail.com>
2024-03-26 09:12:22 +0300

    Enhance task detail page

--- frontend/src/views/TaskDetail.vue
+++ frontend/src/views/TaskDetail.vue
@@ -5,11 +5,35 @@
         v-if="taskDetail"
         class="bg-white overflow-hidden shadow rounded-lg border p-8"
       >
-        <div class="px-4 py-5 sm:px-6">
+        <div class="px-4 py-5 sm:px-6 flex items-center justify-between">
           <h3 class="text-lg leading-6 font-medium text-gray-900">
-            Task Details
+            <router-link
+              class="group flex font-semibold text-sm leading-6 text-slate-700 hover:text-slate-900 dark:text-slate-200 dark:hover:text-white"
+              to="/"
+            >
+              <svg
+                viewBox="0 -9 3 24"
+                class="overflow-visible mr-3 text-slate-400 w-auto h-6 group-hover:text-slate-600 dark:group-hover:text-slate-300"
+              >
+                <path
+                  d="M3 0L0 3L3 6"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+              Go Back To Task List
+            </router-link>
           </h3>
+          <PencilIcon
+            @click="goToUpdatePage(taskDetail.id)"
+            class="h-5 w-5 mx-3 text-gray-500 hover:cursor-pointer"
+            aria-hidden="true"
+          />
         </div>
+
         <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl class="sm:divide-y sm:divide-gray-200">
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -51,7 +75,9 @@ import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import AuthenticatedLayout from "../layouts/AuthenticatedLayout.vue";
 import axiosClient from "../axios";
-
+import { PencilIcon } from "@heroicons/vue/24/outline";
+import { useRouter } from "vue-router";
+const router = useRouter();
 const taskDetail = ref(null);
 const accessToken = localStorage.getItem("accessToken");
 const route = useRoute();
@@ -68,6 +94,10 @@ const fetchTaskDetail = async () => {
   } catch (error) {
     console.error("Error fetching task detail:", error);
   }
+};
+
+const goToUpdatePage = (taskId) => {
+  router.push(`/update-task/${taskId}`);
 };
 
 onMounted(fetchTaskDetail);
